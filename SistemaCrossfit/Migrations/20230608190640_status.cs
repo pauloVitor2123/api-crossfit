9420715a6cf04d9331d0196ee42db29f7eb5b1d5
Hugo Stowasser <hugopstowasser@gmail.com>
2023-06-08 16:07:01 -0300

    Add Migration

---
+++ SistemaCrossfit/Migrations/20230608190640_status.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaCrossfit.Migrations
+{
+    public partial class status : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Status",
+                columns: table => new
+                {
+                    updated_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    deleted_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    id_profile = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    normalized_name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    active = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Status", x => x.id_profile);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Status");
+        }
+    }
+}
