43d1ca98f88fa91dfc650d8601861e7d2a6242a7
Hugo Stowasser <hugopstowasser@gmail.com>
2023-06-08 14:50:14 -0300

    CRUD Status

---
+++ SistemaCrossfit/Factories/StatusFactory.cs
@@ -0,0 +1,39 @@
+﻿using SistemaCrossfit.Models;
+
+namespace SistemaCrossfit.Factories
+{
+    public class StatusFactory
+    {
+        public static List<Status> CreateSeedStatuses()
+        {
+            List<Status> status = new List<Status>();
+
+            Status active = new Status()
+            {
+                Name = "Active",
+                NormalizedName = "ACTIVE",
+                Active = true
+            };
+            status.Add(active);
+
+            Status pendent = new Status()
+            {
+                Name = "Pendent",
+                NormalizedName = "PENDENT",
+                Active = true
+            };
+            status.Add(pendent);
+
+            Status blocked = new Status()
+            {
+                Name = "Blocked",
+                NormalizedName = "BLOCKED",
+                Active = true
+            };
+            status.Add(blocked);
+
+            return status;
+        }
+    }
+
+}
---
+++ SistemaCrossfit/Repositories/Interface/IStatusRepository.cs
@@ -0,0 +1,9 @@
+﻿using SistemaCrossfit.Models;
+
+namespace SistemaCrossfit.Repositories.Interface
+{
+    public interface IStatusRepository : IBaseRepository<Status>
+    {
+        Task<Status> GetByNormalizedName(string normalizedName);
+    }
+}
