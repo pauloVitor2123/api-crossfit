7e3bc2a31a15c9e6e4401ac39b67a5b6e329b105
Paulo <paulo.vitor@jovensgenios.com>
2023-06-20 02:04:35 -0300

    fix: Status mapper

--- SistemaCrossfit/Data/Map/StatusMap.cs
+++ SistemaCrossfit/Data/Map/StatusMap.cs
@@ -9,7 +9,7 @@ namespace SistemaCrossfit.Data.Map
         public override void Configure(EntityTypeBuilder<Status> builder)
         {
             builder.HasKey(x => x.IdStatus);
-            builder.Property(x => x.IdStatus).HasColumnName("id_profile").ValueGeneratedOnAdd();
+            builder.Property(x => x.IdStatus).HasColumnName("id_status").ValueGeneratedOnAdd();
             builder.Property(x => x.Name).IsRequired().HasMaxLength(255).HasColumnName("name");
             builder.Property(x => x.NormalizedName).IsRequired().HasMaxLength(255).HasColumnName("normalized_name");
             builder.Property(x => x.Active).IsRequired().HasColumnName("active");
