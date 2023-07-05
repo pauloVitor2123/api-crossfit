ddb6e0872a0501fb6703426f93acfb02c6896453
Yan Hermanny <88159884+yanhermanny@users.noreply.github.com>
2023-07-05 02:51:17 -0300

    Altera a PK de StudentPoints

---
+++ SistemaCrossfit/Migrations/20230705052020_change-studentPoints-PK.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaCrossfit.Migrations
+{
+    public partial class changestudentPointsPK : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_StudentPoints",
+                table: "StudentPoints");
+
+            migrationBuilder.AddColumn<int>(
+                name: "id_student_points",
+                table: "StudentPoints",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_StudentPoints",
+                table: "StudentPoints",
+                column: "id_student_points");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentPoints_id_student",
+                table: "StudentPoints",
+                column: "id_student");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_StudentPoints",
+                table: "StudentPoints");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StudentPoints_id_student",
+                table: "StudentPoints");
+
+            migrationBuilder.DropColumn(
+                name: "id_student_points",
+                table: "StudentPoints");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_StudentPoints",
+                table: "StudentPoints",
+                columns: new[] { "id_student", "id_exercise" });
+        }
+    }
+}
